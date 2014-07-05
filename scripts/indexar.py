346ddb0e22a089fba03e2bbf3607aa9ae88fec1a
Andres MRM <andresmrm@gmail.com>
2014-07-05 13:22:36 -0300

    Script para fazer Solr indexar CSVs depois de dividir por dia

---
+++ scripts/indexar.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+pasta = "/home/andres/pordia/"
+
+for nome in sorted(os.listdir(pasta)):
+	caminho = pasta + nome
+	link = " 'http://localhost:8080/solr/docsolr/update/csv?stream.file=%s&commit=true&header=true&fieldnames=id,data,retranca,tipo_conteudo,secretaria,orgao,texto&stream.contentType=text/csv'" % caminho
+	r = subprocess.call("curl"+ link, shell=True)
+	print(caminho)
+	print(r)
