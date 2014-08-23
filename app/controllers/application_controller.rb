e13010873bce8d98a720384ca67a74e33a6aa85e
Andres MRM <andresmrm@gmail.com>
2014-08-23 18:13:05 -0300

    rascunho de pagina de sobre e baixar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def sobre
+  end
+
+  def aaa
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sobre' => 'application#sobre'
+  get 'baixar' => 'application#baixar'
+
   root :to => "catalog#index"
   blacklight_for :catalog
   Blacklight::Marc.add_routes(self)
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
