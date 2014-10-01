d0f25498c3765f02d98098fe53f7299920d36961
Rafael Santana <rafa.eu.santana@gmail.com>
2014-10-01 10:57:12 -0300

    Adicionei página sobre busca avançada e tirei date picker para browsers com suporte a campo date

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sobre' => 'application#sobre'
   get 'baixar' => 'application#baixar'
+  get 'buscaavancada' => 'application#buscaavancada'
 
   root :to => "catalog#index"
   blacklight_for :catalog
